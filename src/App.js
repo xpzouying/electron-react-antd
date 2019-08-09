6874d08e1014d2f727eea1a56afafbfe279e5476
zouying <xpzouying@qq.com>
2019-08-09 12:23:01 +0800

    update demo page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { Button } from "antd";
+import { Card } from "antd";
 import "./App.css";
+
+const { Meta } = Card;
 
 function App() {
   return (
     <div className="App">
-      <Button type="primary">Button</Button>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={
+          <img
+            alt="electron-react-antd"
+            src="https://zyblog-1255449766.cos.ap-beijing.myqcloud.com/3152ecb1-f7a8-4a0b-a19f-207400b9c65d"
+          />
+        }
+      >
+        <Meta
+          title="xpzouying@gmail.com"
+          description="Electron With React and Antd"
+        />
+      </Card>
     </div>
   );
 }
